2b4ef77c3bc1cbf25ada66dcd0d0527978673107
Jan Jones <janjones@microsoft.com>
2023-04-21 15:12:44 +0200

    Deserialize LazyIntermediateToken

--- src/Compiler/Microsoft.NET.Sdk.Razor.SourceGenerators/Serialization/RazorContractResolver.cs
+++ src/Compiler/Microsoft.NET.Sdk.Razor.SourceGenerators/Serialization/RazorContractResolver.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Razor.Language.Intermediate;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using System;
 using System.Reflection;
 
 namespace Microsoft.NET.Sdk.Razor.SourceGenerators;
@@ -13,7 +14,17 @@ internal sealed class RazorContractResolver : DefaultContractResolver
 
         if (property.DeclaringType == typeof(LazyIntermediateToken))
         {
-            property.Ignored = true;
+            if (property.PropertyName == nameof(LazyIntermediateToken.Content))
+            {
+                property.Writable = true;
+                property.ValueProvider = new DelegateValueProvider<LazyIntermediateToken, string>(
+                    static token => token.Content,
+                    static (token, content) => token.Content = content);
+            }
+            else
+            {
+                property.Ignored = true;
+            }
         }
 
         if (property.DeclaringType == typeof(DocumentIntermediateNode) && property.PropertyName == nameof(DocumentIntermediateNode.Target))
@@ -22,5 +33,27 @@ internal sealed class RazorContractResolver : DefaultContractResolver
         }
 
         return property;
+    }
+
+    private class DelegateValueProvider<TTarget, TValue> : IValueProvider
+    {
+        private readonly Func<TTarget, TValue?> _getter;
+        private readonly Action<TTarget, TValue?> _setter;
+
+        public DelegateValueProvider(Func<TTarget, TValue?> getter, Action<TTarget, TValue?> setter)
+        {
+            _getter = getter;
+            _setter = setter;
+        }
+
+        public object? GetValue(object target)
+        {
+            return _getter((TTarget)target);
+        }
+
+        public void SetValue(object target, object? value)
+        {
+            _setter((TTarget)target, (TValue?)value);
+        }
     }
 }
