357b636c8dc5c81b08659b472c71f0dfacf791c2
Jan Jones <janjones@microsoft.com>
2023-04-21 17:21:24 +0200

    Support file kind

--- src/Compiler/Microsoft.NET.Sdk.Razor.SourceGenerators/Serialization/RazorParserOptionsConverter.cs
+++ src/Compiler/Microsoft.NET.Sdk.Razor.SourceGenerators/Serialization/RazorParserOptionsConverter.cs
@@ -17,8 +17,9 @@ internal sealed class RazorParserOptionsConverter : JsonConverter<RazorParserOpt
         }
 
         var designTime = reader.ReadPropertyName(nameof(RazorParserOptions.DesignTime)).ReadAsBoolean().GetValueOrDefault();
+        var fileKind = reader.ReadPropertyName(nameof(RazorParserOptions.FileKind)).ReadAsString();
         reader.ReadPropertyName(ValuePropertyName).Read();
-        return designTime ? RazorParserOptions.CreateDesignTime(factory) : RazorParserOptions.Create(factory);
+        return designTime ? RazorParserOptions.CreateDesignTime(factory, fileKind) : RazorParserOptions.Create(factory, fileKind);
 
         void factory(RazorParserOptionsBuilder builder)
         {
@@ -37,14 +38,14 @@ internal sealed class RazorParserOptionsConverter : JsonConverter<RazorParserOpt
         writer.WriteStartObject();
         writer.WritePropertyName(nameof(RazorParserOptions.DesignTime));
         writer.WriteValue(value.DesignTime);
+        writer.WritePropertyName(nameof(RazorParserOptions.FileKind));
+        writer.WriteValue(value.FileKind);
         writer.WritePropertyName(ValuePropertyName);
         writer.WriteStartObject();
         writer.WritePropertyName(nameof(RazorParserOptions.ParseLeadingDirectives));
         writer.WriteValue(value.ParseLeadingDirectives);
         writer.WritePropertyName(nameof(RazorParserOptions.Version));
         writer.WriteValue(value.Version.ToString());
-        writer.WritePropertyName(nameof(RazorParserOptions.FileKind));
-        writer.WriteValue(value.FileKind);
         writer.WritePropertyName(nameof(RazorParserOptions.Directives));
         writer.WriteStartArray();
 
